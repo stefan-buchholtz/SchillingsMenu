474ce59ff7c5b9efbe031b6b5d3e7588c64008b5
Stefan Buchholtz <stbuchholtz@gmail.com>
2014-04-16 15:51:04 +0200

    Bugfixes in iCal generator

--- iCalendar.js
+++ iCalendar.js
@@ -1,17 +1,24 @@
 var sprintfJs = require('sprintf-js');
 
+var ESCAPES = {
+    '\\': '\\\\',
+    '\n': '\\n',
+    ',': '\\,',
+    ';': '\\;'
+}
+
 module.exports = function(prodId, events) {
 	var ical = 'BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:' + prodId + '\n';
 	ical = ical + events.map(function(event) {
 		var s = 'BEGIN:VEVENT\n';
 		if ( event.uid ) {
-			s = s + 'UID:' + event.uid + '\n';
+			s = s + getICalString('UID:', event.uid) + '\n';
 		}
 		if ( event.summary ) {
-			s = s + getICalString('SUMMARY:' + event.summary) + '\n';
+			s = s + getICalString('SUMMARY', event.summary) + '\n';
 		}
 		if ( event.description ) {
-			s = s + getICalString('DESCRIPTION:' + event.description) + '\n';
+			s = s + getICalString('DESCRIPTION', event.description) + '\n';
 		}
 		s = s + 'DTSTAMP:' + getDateString(event.startDate) + '\n';
 		s = s + 'DTSTART:' + getDateString(event.startDate) + '\n';
@@ -20,8 +27,7 @@ module.exports = function(prodId, events) {
 		return s;
 	}).join('');
 	ical = ical + 'END:VCALENDAR\n';
-	ical = ical.replace('\n', '\r\n');
-	return ical;
+	return ical.replace(/\n/g, '\r\n');
 }
 
 function getDateString(date) {
@@ -34,11 +40,15 @@ function getDateString(date) {
 		date.getUTCSeconds());
 }
 
-function getICalString(s) {
-	s = s.replace('\n', '\\n', 'g');
-	var lines = [];
-	for (var i = 0; i < s.length; i += 75) {
-		lines.push(s.substr(i, 75));
-	}
-	return lines.join('\n ');
+function getICalString(key, value) {
+    value = value.split('').map(function(c) {
+	return ESCAPES[c] || c;
+    }).join('');
+
+    var line = key + ':' + value;
+    var lines = [];
+    for (var i = 0; i < line.length; i += 75) {
+	lines.push(line.substr(i, 75));
+    }
+    return lines.join('\n ');
 }
